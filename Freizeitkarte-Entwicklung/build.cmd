58b75c9fff5cb0adfae845c2b6d08c897b79e506
michaeloed <7830883+michaeloed@users.noreply.github.com>
2021-09-10 08:03:53 +0200

    gmappsupp build

--- Freizeitkarte-Entwicklung/build.cmd
+++ Freizeitkarte-Entwicklung/build.cmd
@@ -3,6 +3,7 @@ perl mt.pl -ele=%1 fetch_osm %2
 perl mt.pl -ele=%1 fetch_ele %2
 perl mt.pl -ele=%1 join %2
 perl mt.pl -ele=%1 split %2
-perl mt.pl -ele=%1 build %2
-perl mt.pl -ele=%1 nsis %2
+perl mt.pl -ele=%1 build %2 DEXTENDEDROUTING
+perl mt.pl -ele=%1 nsis %2 DEXTENDEDROUTING
+perl mt.pl -ele=%1 gmapsupp %2 DEXTENDEDROUTING
 pause
--- Freizeitkarte-Entwicklung/build_fenix.cmd
+++ Freizeitkarte-Entwicklung/build_fenix.cmd
@@ -3,6 +3,7 @@ perl mt.pl -ele=%1 -typfile=%cd%\TYP\%3.typ fetch_osm %2
 perl mt.pl -ele=%1 -typfile=%cd%\TYP\%3.typ fetch_ele %2
 perl mt.pl -ele=%1 -typfile=%cd%\TYP\%3.typ join %2
 perl mt.pl -ele=%1 -typfile=%cd%\TYP\%3.typ split %2
-perl mt.pl -ele=%1 -typfile=%cd%\TYP\%3.typ build %2
-perl mt.pl -ele=%1 -typfile=%cd%\TYP\%3.typ nsis %2
+perl mt.pl -ele=%1 -typfile=%cd%\TYP\%3.typ build %2 DEXTENDEDROUTING
+perl mt.pl -ele=%1 -typfile=%cd%\TYP\%3.typ nsis %2 DEXTENDEDROUTING
+perl mt.pl -ele=%1 -typfile=%cd%\TYP\%3.typ gmapsupp %2 DEXTENDEDROUTING
 pause
