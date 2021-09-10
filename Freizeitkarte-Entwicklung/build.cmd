285a5f300464ef1cd3a931b63b7df46fde78a003
michaeloed <7830883+michaeloed@users.noreply.github.com>
2021-09-10 15:58:00 +0200

    Add DEM data for germany, alps and italy

--- Freizeitkarte-Entwicklung/build.cmd
+++ Freizeitkarte-Entwicklung/build.cmd
@@ -1,9 +1,11 @@
-perl mt.pl -ele=%1 create %2
-perl mt.pl -ele=%1 fetch_osm %2
-perl mt.pl -ele=%1 fetch_ele %2
-perl mt.pl -ele=%1 join %2
-perl mt.pl -ele=%1 split %2
-perl mt.pl -ele=%1 build %2 DEXTENDEDROUTING
-perl mt.pl -ele=%1 nsis %2 DEXTENDEDROUTING
-perl mt.pl -ele=%1 gmapsupp %2 DEXTENDEDROUTING
+set DEMPATH=g:/osm/fzk-mde-garmin/Freizeitkarte-Entwicklung/dem/hgt/
+
+perl mt.pl --ele=%1 --dempath=%DEMPATH% create %2
+perl mt.pl --ele=%1 --dempath=%DEMPATH% fetch_osm %2
+perl mt.pl --ele=%1 --dempath=%DEMPATH% fetch_ele %2
+perl mt.pl --ele=%1 --dempath=%DEMPATH% join %2
+perl mt.pl --ele=%1 --dempath=%DEMPATH% split %2
+perl mt.pl --ele=%1 --dempath=%DEMPATH% build %2 DEXTENDEDROUTING
+perl mt.pl --ele=%1 nsis %2 DEXTENDEDROUTING
+perl mt.pl --ele=%1 gmapsupp %2 DEXTENDEDROUTING
 pause
